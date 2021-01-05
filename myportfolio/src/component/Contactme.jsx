2bf487ebeba22256e8b8b8b6d64ff5659a25cd27
TieuKhang <52084792+TieuKhang@users.noreply.github.com>
2021-01-05 13:30:38 -0500

    Update

--- myportfolio/src/component/Contactme.jsx
+++ myportfolio/src/component/Contactme.jsx
@@ -7,11 +7,16 @@ const Contactme = (prop) => {
             <p> Feel free to reach out to me through: </p>
             <p> University email: k4tieu@uwaterloo.ca </p>
             <p> Phone number: 437 929 7617 </p>
-            <p> My projects code is available on Github : <a href = "https://github.com/TieuKhang">
-               https://github.com/TieuKhang </a> 
+            <p> My projects code is available on Github : 
+                <a style={{display: "table-cell"}} target = "_blank" rel = "noopener noreferrer"
+                   href = "https://github.com/TieuKhang">
+                    https://github.com/TieuKhang 
+                </a> 
             </p>
-            <p> My Linkedin: <a href = "https://www.linkedin.com/in/khang-tieu-773636191/">
-                https://www.linkedin.com/in/khang-tieu-773636191/
+            <p> My Linkedin: 
+                <a style={{display: "table-cell"}} target = "_blank" rel = "noopener noreferrer"
+                   href = "https://www.linkedin.com/in/khang-tieu-773636191/">
+                    https://www.linkedin.com/in/khang-tieu-773636191/
                 </a>
             </p>
         </div>
